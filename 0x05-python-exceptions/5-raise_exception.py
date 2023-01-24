ee5d6ba2aef26e0b1bf12661dbd503c436fe2f6b
Emirate111 <emmanuelanyaigbo@gmail.com>
2023-01-24 09:54:24 -0800

    Python exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
+
+
+
+
+vi 6-raise_exception_msg.py
+
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError(message)
